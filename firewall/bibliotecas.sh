df306da1bf604c2db07c0a0a1da4d05a26c391f3
Gabriel Ribeiro <gabrielribbeirodematos@gmail.com>
2022-12-17 07:13:14 -0300

    Update bibliotecas.sh

--- firewall/bibliotecas.sh
+++ firewall/bibliotecas.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+function regras(){
+    read -p "Deseja configurar: [INPUT,OUTPUT,FORWARD]" entrada
+    read -p "Informe  o protocolo que deseja configurar: [tcp,udp,icmp]" protocolo
+    read -p "Informe a porta do protocolo: [ex:22,80,53..]" porta
+    read -p "Qual ação deseja realizar: [REJECT,DROP, ACCEPT]" acao
+    echo -e '\t Realizando configuração...';sleep 2s; clear
+    $(iptables -A "$entrada" -p "$protocolo" --dport "$porta" -j "$acao")
+    echo "Firewall configuado"
+}
